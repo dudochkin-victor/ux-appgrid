33f5d6f7d84c708870e434d3796e8b506adebbd3
Victor Dudochkin <dudochkin.victor@gmail.com>
2013-05-15 14:53:08 +0800

    Initial commit

---
+++ ts/meego-ux-appgrid.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>MainContent</name>
+    <message>
+        <location filename="../../MainContent.qml" line="175"/>
+        <source>Delete &quot;%1&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainContent.qml" line="190"/>
+        <source>Deleting &quot;%1&quot; will also delete all of its data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainContent.qml" line="190"/>
+        <source>&quot;%1&quot; can not be deleted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainContent.qml" line="204"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainContent.qml" line="216"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainContent.qml" line="661"/>
+        <source>Personalize</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
